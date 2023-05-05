3bd7ca2070358fa10c776533c11e0147ef4b32e6
Nagy Janos <nagy@zold.lan>
2023-05-05 11:59:44 +0200

    Belépés kész

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const doc = {
     tbody: document.querySelector('#tbody'),
-    operatorButton: document.querySelector('#operatorButton')
+    operatorButton: document.querySelector('#operatorButton'),
+    showLoginButton: document.querySelector('#showLoginButton'),
+    loginButton: document.querySelector('#loginButton'),
+    userInput: document.querySelector('#user'),
+    passInput: document.querySelector('#pass'),
+    loginModal: new bootstrap.Modal('#loginModal'),
+    table: document.querySelector('#table'),
+    showAddButton: document.querySelector('#showAddButton'),
+    idInput: document.querySelector('#id'),
+    nameInput: document.querySelector('#name'),
+    cityInput: document.querySelector('#city'),
+    salaryInput: document.querySelector('#salary')
 }
 
 const state = {
     employees: [],
-    host: 'http://localhost:8000/api/'
+    host: 'http://localhost:8000/api/',
+    logedin: false,
+    adding: true
 }
 
 window.addEventListener('load', () => {
@@ -18,11 +31,73 @@ function init() {
     doc.operatorButton.addEventListener('click', () => {
         startOperation();
     });
-    getEmployees()    
+    doc.loginButton.addEventListener('click', () => {
+        startLogin();
+    });
+    changeVisible()
+    // getEmployees()    
 }
 
 function startOperation() {
     console.log('működik')
+    let employee = {
+        // name: doc.
+    }
+}
+
+function startLogin() {
+    console.log('Azonosítás...')
+    let userData = {
+        name: doc.userInput.value,
+        password: doc.passInput.value
+    }
+    login(userData)
+}
+
+function login(userData) {
+    let endpoint = 'login'
+    let url = state.host + endpoint
+    let headers = {
+        'Content-Type': 'application/json'
+    }
+    fetch(url, {
+        method: 'post',
+        headers: headers,
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(result => {
+
+        localStorage.setItem('token', result.token)
+        localStorage.setItem('name', result.name)
+        doc.loginModal.hide()
+        doc.userInput.value = ''
+        doc.passInput.value = ''
+        state.logedin = true
+        changeVisible()
+    })
+}
+
+function changeVisible() {
+    state.logedin= checkLogedin()
+
+    if(state.logedin) {
+        doc.showLoginButton.classList.add('invisible')
+        doc.showAddButton.classList.remove('invisible')
+        doc.table.classList.remove('invisible')
+    }else {
+        doc.showLoginButton.classList.remove('invisible')
+        doc.showAddButton.classList.add('invisible')
+        doc.table.classList.add('invisible')
+    }
+}
+//TODO: A token érvényességét ellenőrizni kell
+function checkLogedin() {
+    if(localStorage.getItem('token')) {
+        return true;
+    }else {
+        return false;
+    }    
 }
 
 function getEmployees() {
